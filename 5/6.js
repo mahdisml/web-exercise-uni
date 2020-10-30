064c9828a9ee5e2de4882099523a4a1d299d096f
MahdiSml <mahdisml98@gmail.com>
2020-10-30 14:36:39 +0330

    js is shit

--- 5/6.js
+++ 5/6.js
@@ -1,54 +1,64 @@
-let readln = require('readline');
-let cl = readln.createInterface(process.stdin, process.stdout);
+var readline = require('readline');
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
 
-let read = function() {
-    return new Promise((res, _) => {
-        cl.on('line', answer => {
-            res(answer);
-        })
-    });
-};
-(async function main() {
-    let nm = (await read()).split(" ");
-    let n = parseInt(nm[0]);
-    let m = parseInt(nm[1]);
-    let data = [];
-    let lNumbers = 0;
+let cnt = -1;
+let n;
+let m;
+let data = [];
+let lNumbers = 0;
 
-    for(let i = 0; i < n; i++){
-        data[i] = await read();
+rl.on('line', function (line) {
+
+    if (cnt === -1){
+        let nm = line.split(' ');
+        n = parseInt(nm[0]);
+        m = parseInt(nm[1]);
+        cnt++;
     }
-    if (n > 0 && m < 101) {
-        for (let i = 0; i < n - 1; i++) {
-            for (let j = 0; j < m - 1; j++) {
-                if (data[i][j] === "*") {
-                    isL(i, j)
+    else{
+        if (cnt < n - 1){
+            data[cnt] = line;
+            cnt++;
+        } else{
+            if (n > 0 && m < 101) {
+                data[cnt] = line;
+                cnt++;
+                for (let i = 0; i < n - 1; i++) {
+                    for (let j = 0; j < m - 1; j++) {
+                        if (data[i][j] === "*") {
+                            isL(i, j)
+                        }
+                    }
                 }
             }
-        }
-    }
-    function isL(a,b){
-        let k1 = 1;
-        for(let i = a; i < n; i++){
-            if (k1 > 0 && data[i][b] === "*") {
-                let k2 = 1;
-                for(let j = b; j < m; j++){
-                    if ((k1 === (2*k2)) && (k2 > 1) && (data[i][j] === "*")){
-                        lNumbers++
+            function isL(a,b){
+                let k1 = 1;
+                for(let i = a; i < n; i++){
+                    if (k1 > 0 && data[i][b] === "*") {
+                        let k2 = 1;
+                        for(let j = b; j < m; j++){
+                            if ((k1 === (2*k2)) && (k2 > 1) && (data[i][j] === "*")){
+                                lNumbers++
+                            }
+                            if (k2>1 && data[i][j] === "."){
+                                break;
+                            }
+                            k2++;
+                        }
                     }
-                    if (k2>1 && data[i][j] === "."){
+                    if (k1 > 1 && data[i][b] === "."){
                         break;
                     }
-                    k2++;
+                    k1++;
                 }
+                return false;
             }
-            if (k1 > 1 && data[i][b] === "."){
-                break;
-            }
-            k1++;
+            console.log(lNumbers)
         }
-        return false;
     }
-    console.log(lNumbers)
-    cl.close();
-})();
+    }
+)
