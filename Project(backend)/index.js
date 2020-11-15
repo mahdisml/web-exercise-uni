b920bb6962d19c56d6dfe6da768fbf47d599a057
pooyasp2 <pooyasharifi.sp2@gmail.com>
2020-11-15 12:23:54 +0330

    add item to backend(Logic)

--- Project(backend)/index.js
+++ Project(backend)/index.js
@@ -3,11 +3,38 @@ const express = require('express')
 const app = express()
 const port = 3000
 const hbs = require('hbs')
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use(express.static('public'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => res.render('index'))
+app.get('/manageitem', (req, res) =>res.render('manageitem'))
+let data =[
+  {
+    id: 1,
+    name: 'lorem',
+    price: 300
+  },
+  {
+    id:2,
+    name: 'lorem2',
+    price: 350
+  },
+]
+app.post('/additem', (req, res) => {
+    lid = 
+    data.push(
+        {
+            id: data[data.length - 1].id + 1,
+            name: req.body.name,
+            cost: parseInt(req.body.price)
+        })
+    console.log(data)
+    res.render('index', data)
+})
 
 
 app.listen(port, () => {
