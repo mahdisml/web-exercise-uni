8e608d16e4d6168c9f96fce1f43110d142c23a8b
MahdiSml <mahdisml98@gmail.com>
2020-11-25 20:07:28 +0330

    start of new Project !

--- Project(backend)/index.js
+++ Project(backend)/index.js
@@ -1,42 +1,69 @@
-const path = require('path')  
+const path = require('path')
 const express = require('express')
 const app = express()
 const port = 3000
-const hbs = require('hbs')
 const bodyParser = require("body-parser");
+const handlebars = require('express-handlebars');
+const fs = require("fs");
+let data;
+
+fs.readFile(path.join(__dirname, '/items.json'), (err, newData) => {
+    if (err) throw err;
+    data = JSON.parse(newData);
+    console.log(data);
+});
+
+function updateJson(){
+    let newData = JSON.stringify(data, null, 2);
+    fs.writeFile(path.join(__dirname, '/items.json'), newData, (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+    });
+}
+
 app.use(bodyParser.urlencoded({ extended: true }))
-
-app.use(express.static('public'))
-app.set('views', path.join(__dirname, 'views'))
+app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views',
+    extname: 'hbs'
+}));
+app.use(express.static(path.join(__dirname, '/public')))
+app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'hbs')
-
-app.get('/', (req, res) => res.render('index'))
-app.get('/manageitem', (req, res) =>res.render('manageitem'))
-let data =[
-  {
-    id: 1,
-    name: 'lorem',
-    price: 300
-  },
-  {
-    id:2,
-    name: 'lorem2',
-    price: 350
-  },
-]
-app.post('/additem', (req, res) => {
-    lid = 
-    data.push(
-        {
-            id: data[data.length - 1].id + 1,
-            name: req.body.name,
-            cost: parseInt(req.body.price)
-        })
-    console.log(data)
-    res.render('index', data)
-})
-
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+app.get('/', (req, res) => {
+    res.render('main',{data:data})
+})
+app.get('/forooshgah', (req, res) =>{
+    res.render('main',{})
+})
+app.get('/modiriatMahsool', (req, res) =>{
+    res.render('main',{})
+})
+app.get('/akharinMahsool', (req, res) =>{
+    res.render('main',{})
+})
+app.get('/blog', (req, res) =>{
+    res.render('main',{})
+})
+app.get('/safheha', (req, res) => {
+    res.render('main',{})
+})
+app.get('/ertebatBaMa', (req, res) => {
+    res.render('main',{})
+})
+
+// app.get('/manageitem', (req, res) =>res.render('manageitem'))
+// app.post('/additem', (req, res) => {
+//     let lid =
+//         data.push({
+//             id: data[data.length - 1].id + 1,
+//             name: req.body.name,
+//             cost: parseInt(req.body.price)
+//         })
+//     console.log(data)
+//     res.render('index', data)
+// })
